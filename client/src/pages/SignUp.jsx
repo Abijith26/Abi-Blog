50be297687995641387d26ff2ca4c6b1428d25e7
Abijith26 <abimurugan.c@gmail.com>
2024-02-17 14:50:44 +0530

    Added signup functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,22 +1,60 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Button, Label, TextInput } from "flowbite-react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 
 import pic from "../assets/Sign_up.png";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  function handleChange(e) {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fileds");
+    }
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setIsLoading(false);
+      if (res.ok) {
+        navigate("/signin");
+      }
+    } catch (error) {
+      setErrorMessage(error.message);
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="min-h-screen mt-20 ml-10">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
         {/* Left Side Div */}
         <div className="flex-1">
           <Link to="/" className=" font-bold dark: text-black text-4xl">
-            <span className="px-2 py-1 bg-gradient-to-r from-blue-500 via-pink-400 to-yellow-200 rounded-lg">
+            <span className="px-2 py-1 bg-blue-500 bg-opacity-75 rounded-lg">
               Blog
             </span>
           </Link>
           <p className="text-sm mt-5">
-            Ready to make your keyboard dance? Sign in and let's blog!
+            Ready to make your keyboard dance? Sign Up
           </p>
         </div>
         {/* <div className="mt-10 relative w-50 h-50 sm:w-screen sm:h-screen">
@@ -25,21 +63,43 @@ export default function SignUp() {
 
         {/* Right side Div */}
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
             <div className="">
               <Label value="Your username" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your email" />
-              <TextInput type="text" placeholder="abc@mail.com" id="email" />
+              <TextInput
+                type="email"
+                placeholder="abc@mail.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div className="">
               <Label value="Your password" />
-              <TextInput type="text" placeholder="password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button color="blue" type="submit">
-              Sign Up
+            <Button color="blue" type="submit" disabled={isLoading}>
+              {isLoading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading....</span>
+                </>
+              ) : (
+                "Sign Up"
+              )}
             </Button>
           </form>
           <div className="flex gap-3 mt-3 text-sm ">
@@ -48,6 +108,12 @@ export default function SignUp() {
               Sign-In
             </Link>
           </div>
+
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
