a99e29bd42ef7b2fe9ed778922cf9f259bf53e3d
Abijith26 <abimurugan.c@gmail.com>
2024-02-26 16:41:00 +0530

    Code Altered

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,18 @@
 import { Button, TextInput } from "flowbite-react";
-import React from "react";
+import {} from "react";
 import { useSelector } from "react-redux";
+
 export default function Dashboard() {
+  // accessing the data from the global store (redux)
   const { currentUser } = useSelector((store) => store.user);
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
         <div className="w-32 h-32 self-center cursor-pointer overflow-hidden rounded-full">
           <img
-            src={currentUser.profilePicture}
+            src={imageFileURL || currentUser.profilePicture}
             alt="user_profile_picture"
             className="rounded-full w-full h-full object-cover border-8 border-[lightgray]"
           />
